0c159b5eda2c217272945b6354a85a0d6971a55c
Sai Vanamala <saivanamala714@gmail.com>
2018-06-01 20:21:09 -0400

    Initial commit

---
+++ www.awsquickstart.com/src/main/java/AWSQuickStart/www/awsquickstart/com/App.java
@@ -0,0 +1,15 @@
+package AWSQuickStart.www.awsquickstart.com;
+
+/**
+ * Hello world!
+ *
+ */
+/**
+ * @author admin
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
